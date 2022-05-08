f325373c54955e3f62fa3cdfc9848fc041c537c1
juneagain <juneagain@mail.ru>
2022-05-09 02:17:14 +0300

    feat: add routes for forms

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
 import Layout from './components/Layout';
-import BoardPage from './pages/BoardPage';
-import ErrorPage from './pages/ErrorPage';
-import MainPage from './pages/MainPage';
 import WelcomePage from './pages/WelcomePage';
-import './index.scss';
+import MainPage from './pages/MainPage';
+import BoardPage from './pages/BoardPage';
+import LoginForm from './components/LoginForm/LoginForm';
+import SignUpForm from './components/SignUpForm/SignUpForm';
+import ErrorPage from './pages/ErrorPage';
+import theme from './theme';
 import './reset.scss';
+import './index.scss';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -16,15 +20,20 @@ import '@fontsource/roboto/700.css';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<WelcomePage />} />
-          <Route path='main' element={<MainPage />} />
-          <Route path='board' element={<BoardPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<WelcomePage />} />
+            <Route path='main' element={<MainPage />} />
+            <Route path='board' element={<BoardPage />} />
+            <Route path='login' element={<LoginForm />} />
+            <Route path='signup' element={<SignUpForm />} />
+            {/* <Route path='edit-profile' element={<EditProfileForm />} /> */}
+            <Route path='*' element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 );
