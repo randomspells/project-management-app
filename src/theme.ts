4f5d11d09f0db3d1b60f6e3d36fa7272a6cf60f3
juneagain <juneagain@mail.ru>
2022-05-09 02:54:54 +0300

    refactor: wrap theme into responsive font sizes

--- src/theme.ts
+++ src/theme.ts
@@ -1,22 +1,24 @@
-import { createTheme } from '@mui/material';
+import { createTheme, responsiveFontSizes } from '@mui/material';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#006064',
-      light: '#428e92',
-      dark: '#00363a',
+const theme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      primary: {
+        main: '#006064',
+        light: '#428e92',
+        dark: '#00363a',
+      },
+      secondary: {
+        main: '#f44336',
+        light: '#ff7961',
+        dark: '#ba000d',
+      },
+      text: {
+        primary: '#fff',
+        secondary: '#000',
+      },
     },
-    secondary: {
-      main: '#f44336',
-      light: '#ff7961',
-      dark: '#ba000d',
-    },
-    text: {
-      primary: '#fff',
-      secondary: '#000'
-    }
-  },
-});
+  }),
+);
 
 export default theme;
