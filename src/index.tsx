a080b8903be8568d79f89d2da205a7e48d8b32a5
juneagain <juneagain@mail.ru>
2022-05-08 16:41:02 +0300

    refactor: install roboto font for MUI and reset font settings

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,10 @@ import MainPage from './pages/MainPage';
 import WelcomePage from './pages/WelcomePage';
 import './index.scss';
 import './reset.scss';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
