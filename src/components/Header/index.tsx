deaf3627be8196edf9bd1344207f2410f002c4b6
ViktorElenich <89132609+ViktorElenich@users.noreply.github.com>
2022-05-10 10:45:05 +0300

    feat: add header and animation scroll

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,63 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import styles from './header.module.scss';
 
-const Header: FC = () => (
-  <div>
-    <button type='button'>Sign out</button>
-    <button type='button'>Create board</button>
-  </div>
-);
+const Header: FC = () => {
+  const [sticky, setSticky] = useState({ isSticky: false, offset: 0 });
+  const headerRef = useRef<null | HTMLDivElement>(null);
+
+  const handleScroll = (elTopOffset: number, elHeight: number) => {
+    if (window.pageYOffset > (elTopOffset + elHeight)) {
+      setSticky({ isSticky: true, offset: elHeight });
+    } else {
+      setSticky({ isSticky: false, offset: 0 });
+    }
+  };
+
+  useEffect(() => {
+    const header = headerRef.current?.getBoundingClientRect();
+    const handleScrollEvent = () => {
+      handleScroll(header!.top, header!.height)
+    }
+ 
+    window.addEventListener('scroll', handleScrollEvent);
+ 
+    return () => {
+      window.removeEventListener('scroll', handleScrollEvent);
+    };
+  }, []);
+
+  return (
+    <header
+    id="sticky-header"
+    className={`${styles.navbar}${sticky.isSticky ? ` ${ styles.headerContainer}` : ''}`}
+    ref={headerRef}
+    >
+      <div className={styles.board}>
+        <Button>Create board</Button>
+      </div>
+      <div className={styles.headerOption}>
+        <Button>Edit profile</Button>
+        <Button>Sign out</Button>
+        <FormControl sx={{ m: 1, minWidth: 80 }} size="small">
+          <InputLabel id="demo-simple-select-autowidth-label" className={styles.primary}>Lang</InputLabel>
+          <Select
+            label="Lang"
+            autoWidth
+            labelId="demo-simple-select-autowidth-label"
+            id="demo-simple-select-autowidth"
+          >
+            <MenuItem value="ru">Ru</MenuItem>
+            <MenuItem value="en">En</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    </header>
+  )
+};
 
 export default Header;
