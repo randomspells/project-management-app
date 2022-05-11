c1337ff0648fa14a70bd7ea2563d6c4efcae0272
juneagain <juneagain@mail.ru>
2022-05-11 03:12:10 +0300

    feat: add interfaces and utils function

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -6,3 +6,30 @@ export interface SignInInterface {
 export interface SignUpInterface extends SignInInterface {
   name: string;
 }
+
+export interface FileInterface {
+  filename: string;
+  fileSize: number;
+}
+
+export interface TaskInterface {
+  id: string;
+  title: string;
+  order: number;
+  done: boolean;
+  description: string;
+  userId: string;
+  files: FileInterface[];
+}
+
+export interface ColumnInterface {
+  id: string;
+  title: string;
+  order: number;
+  tasks: TaskInterface[];
+}
+
+export interface BoardInterface {
+  id: string;
+  title: string;
+}
