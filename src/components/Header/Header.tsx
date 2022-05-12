eb6ed40fdb7de736bebf96cebef891af4a50b84d
juneagain <juneagain@mail.ru>
2022-05-13 02:48:29 +0300

    feat: add new board form near opening button with it's own isOpen state

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,15 +4,24 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
+import { useAppDispatch } from '../../hooks/index';
+import { toggleNewBoardForm } from '../../slices/formsSlice';
 import styles from './Header.module.scss';
+import NewBoardForm from '../NewBoardForm/NewBoardForm';
 
 const Header: FC = () => {
-  const {navbar, headerContainer, board, headerOption, primary} = styles;
+  const { navbar, headerContainer, board, headerOption, primary } = styles;
   const [sticky, setSticky] = useState({ isSticky: false, offset: 0 });
   const headerRef = useRef<null | HTMLDivElement>(null);
 
+  const dispatch = useAppDispatch();
+
+  const handleNewBoardClick = () => {
+    dispatch(toggleNewBoardForm());
+  };
+
   const handleScroll = (elTopOffset: number, elHeight: number) => {
-    if (window.pageYOffset > (elTopOffset + elHeight)) {
+    if (window.pageYOffset > elTopOffset + elHeight) {
       setSticky({ isSticky: true, offset: elHeight });
     } else {
       setSticky({ isSticky: false, offset: 0 });
@@ -22,43 +31,37 @@ const Header: FC = () => {
   useEffect(() => {
     const header = headerRef.current?.getBoundingClientRect();
     const handleScrollEvent = () => {
-      handleScroll(header!.top, header!.height)
-    }
- 
+      handleScroll(header!.top, header!.height);
+    };
+
     window.addEventListener('scroll', handleScrollEvent);
- 
+
     return () => {
       window.removeEventListener('scroll', handleScrollEvent);
     };
   }, []);
 
   return (
-    <header
-    id="sticky-header"
-    className={`${navbar}${sticky.isSticky ? ` ${headerContainer}` : ''}`}
-    ref={headerRef}
-    >
+    <header id='sticky-header' className={`${navbar}${sticky.isSticky ? ` ${headerContainer}` : ''}`} ref={headerRef}>
       <div className={board}>
-        <Button>Create board</Button>
+        <Button onClick={handleNewBoardClick}>Create board</Button>
+        <NewBoardForm />
       </div>
       <div className={headerOption}>
         <Button>Edit profile</Button>
         <Button>Sign out</Button>
-        <FormControl sx={{ m: 1, minWidth: 80 }} size="small">
-          <InputLabel id="demo-simple-select-autowidth-label" className={primary}>Lang</InputLabel>
-          <Select
-            label="Lang"
-            autoWidth
-            labelId="demo-simple-select-autowidth-label"
-            id="demo-simple-select-autowidth"
-          >
-            <MenuItem value="ru">Ru</MenuItem>
-            <MenuItem value="en">En</MenuItem>
+        <FormControl sx={{ m: 1, minWidth: 80 }} size='small'>
+          <InputLabel id='demo-simple-select-autowidth-label' className={primary}>
+            Lang
+          </InputLabel>
+          <Select label='Lang' autoWidth labelId='demo-simple-select-autowidth-label' id='demo-simple-select-autowidth'>
+            <MenuItem value='ru'>Ru</MenuItem>
+            <MenuItem value='en'>En</MenuItem>
           </Select>
         </FormControl>
       </div>
     </header>
-  )
+  );
 };
 
 export default Header;
