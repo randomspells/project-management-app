0d5a8c255e6887f876fdd9c23c228cfd3a2281a6
juneagain <juneagain@mail.ru>
2022-05-13 16:08:30 +0300

    feat: add Redux slice for board

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import formsReducer from './slices/formsSlice';
 import taskReducer from './slices/taskSlice';
+import boardReducer from './slices/boardSlice';
 
 export const store = configureStore({
   reducer: {
     forms: formsReducer,
     task: taskReducer,
+    board: boardReducer,
   },
 });
 
