da173cfe233139084fbf68ddf6c6038360dd1617
juneagain <juneagain@mail.ru>
2022-05-13 06:08:48 +0300

    refactor: move modal forms to layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,20 @@
 import { Box } from '@mui/material';
 import React, { FC } from 'react';
 import { Outlet } from 'react-router-dom';
+import EditTaskForm from '../EditTaskForm/EditTaskForm';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import NewBoardForm from '../NewBoardForm/NewBoardForm';
+import NewTaskListForm from '../NewTaskListForm/NewTaskListForm';
 
 const Layout: FC = () => (
   <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh', rowGap: 2 }}>
     <Header />
     <Outlet />
     <Footer />
+    <NewBoardForm />
+    <NewTaskListForm />
+    <EditTaskForm />
   </Box>
 );
 
