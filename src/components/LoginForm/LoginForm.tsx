20a9746b8f07504fd3ad7f0608e5880185072f92
Andrey S. Kochergin <kchrgn@gmail.com>
2022-05-17 21:37:09 +0300

    refactor: use controlled_input in login form

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,46 +1,53 @@
 import React, { FC } from 'react';
-import { Container, TextField, Button, Link, Typography, Box, Avatar } from '@mui/material';
+import { Container, Button, Link, Typography, Box, Avatar } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { useForm } from 'react-hook-form';
+import ControlledInput from '../Inputs/ControlledInput/ControlledInput';
+import { FormDataInterface } from '../../interfaces';
 
-const LoginForm: FC = () => (
-  <Container component="form" maxWidth='xs' sx={{ height: '100%' }}>
-    <Box sx={{mt: 5, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <Avatar sx={{ m: 2, bgcolor: 'primary.main' }}>
-        <LockOutlinedIcon />
-      </Avatar>
-      <Typography component="h1" variant="h5">
-        Login
-      </Typography>
-      <TextField
-        error = {!false}
-        helperText="This field can't be empty"
-        margin="normal"
-        required
-        fullWidth
-        id="login"
-        label="Login"
-        name="login"
-        autoComplete="username"
-        autoFocus
+const LoginForm: FC = () => {
+  const { handleSubmit, control, formState: { isValid }} = useForm({ mode: 'onChange' });
+
+  const onSubmit = (data: FormDataInterface) => {
+    console.log(data);
+  }
+
+  return (
+    <Container component="form" maxWidth='xs' sx={{ height: '100%' }} onSubmit={handleSubmit(onSubmit)}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Avatar sx={{ m: 2, bgcolor: 'primary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <ControlledInput
+          name="login"
+          label="Login"
+          type="text"
+          rules={{ required: true }}
+          errorText="This field can`t be empty"
+          defaultValue=""
+          control={control}
         />
-      <TextField
-        error = {false}
-        margin="normal"
-        required
-        fullWidth
-        id="password"
-        label="Password"
-        name="password"
-        autoComplete="current-password"
-        /> 
-      <Button type="submit" fullWidth variant="contained" size="large" sx={{ mt: 3, mb: 2 }}>
-        Login
-      </Button>
-      <Link href="#signup" variant="body2" sx={{ mx: 'auto' }}>
-        {`Don't have an account? Sign Up`}
-      </Link>
-    </Box>
-  </Container> 
+        <ControlledInput
+          name="password"
+          label="Password"
+          type="text"
+          rules={{ required: true }}
+          errorText="This field can`t be empty"
+          defaultValue=""
+          control={control}
+        />
+        <Button type="submit" fullWidth variant="contained" size="large" sx={{ mt: 3, mb: 2 }} disabled={!isValid}>
+          Login
+        </Button>
+        <Link href="#signup" variant="body2" sx={{ mx: 'auto' }}>
+          {`Don't have an account? Sign Up`}
+        </Link>
+      </Box>
+    </Container> 
   )
+};
 
 export default LoginForm
