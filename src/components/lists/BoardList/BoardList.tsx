16cb2dc198cdfea7f72b9a73472997467e712d18
Viktor <velenic@mail.ru>
2022-05-30 17:33:39 +0300

    feat: add search title board

--- src/components/lists/BoardList/BoardList.tsx
+++ src/components/lists/BoardList/BoardList.tsx
@@ -4,7 +4,11 @@ import Board from '../../cards/Board/Board';
 import { useGetBoardsQuery } from '../../../api/board.api';
 import Loader from '../../other/Loader/Loader';
 
-const BoardList: FC = () => {
+interface BoardListProps {
+  searchValue: string;
+}
+
+const BoardList: FC<BoardListProps> = ({ searchValue }) => {
   const { data: boards = [], isLoading } = useGetBoardsQuery();
 
   return (
@@ -21,7 +25,12 @@ const BoardList: FC = () => {
       }}
     >
       {isLoading && <Loader />}
-      {boards.map((board) => {
+      {boards.filter((board) => {
+        if (searchValue.length > 0) {
+          return board.title.toLowerCase().includes(searchValue.toLowerCase());
+        }
+        return true;
+      }).map((board) => {
         const { id, title, description } = board;
         return (
           <Grid item component='li' key={id} xs={12} md={6} lg={3}>
